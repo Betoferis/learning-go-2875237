87e044c6f5e9df1e646ab2b2ba92bd1d150818e0
Betoferis <betoferis247@gmail.com>
2024-06-25 00:06:07 -0400

    -first attempt

--- practice/main.go
+++ practice/main.go
@@ -1,62 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"strings"
 )
 
-
-// Define a struct
-type Person struct {
-	Name string
-	Age  int
-}
-
-func say(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
+func mergeAlternately(word1 string, word2 string) string {
+    letters_word1 := strings.Split(word1,"")
+    letters_word2 := strings.Split(word2,"")
+    var list []string 
+	i:=0 
+	for (i < len(letters_word1) && i < len(letters_word2)) {
+		list = append(list, letters_word1[i])
+        list = append(list, letters_word2[i])
+		i++;
 	}
+	if (len(letters_word1) > len(letters_word2)){
+		for i:= len(letters_word2) ; i < len(letters_word1) ; i++ {
+            list = append(list, letters_word1[i])
+    }} else {
+		for i:= len(letters_word1) ; i < len(letters_word2) ; i++ {
+            list = append(list, letters_word2[i])
+    }}
+	return (strings.Join(list,""))
 }
 
 func main() {
-	fmt.Println("Hello from Go!")
-
-	var a string = "Hello"
-	var b, c int = 1, 2
-	d := 3.5 // Type inference
-	fmt.Println(a, b, c, d)
-
-	// If-else statement
-	if x := 42; x > 10 {
-		fmt.Println("x is greater than 10")
-	} else {
-		fmt.Println("x is less than or equal to 10")
-	}
-
-	// For loop
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
-	}
-
-	// Arrays
-	var arr [3]int = [3]int{1, 2, 3}
-	fmt.Println(arr)
-
-	// Slices
-	slice := []int{1, 2, 3, 4, 5}
-	fmt.Println(slice)
-
-	// Map declaration
-	m := make(map[string]int)
-	m["one"] = 1
-	m["two"] = 2
-	fmt.Println(m)
-
-	// Create an instance of the struct
-	p := Person{Name: "Alice", Age: 30}
-	fmt.Println(p)
-
-	go say("Hello")
-	say("World")
+	word1 := "abc"
+    word2 := "pqr"
+    result := mergeAlternately(word1, word2)
+    print(result)
 }
